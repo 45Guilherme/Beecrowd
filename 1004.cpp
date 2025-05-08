e88430d963e6906ffa908e721c99631f70d98a02
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-05-08 06:43:44 -0300

    Primeiro commit

---
+++ 1004.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+ 
+    int A;
+    int B;
+    int PROD;
+    
+    printf("");
+    scanf("%d", &A);
+    
+    printf("");
+    scanf("%d", &B);
+    
+    PROD= A*B;
+    
+    printf("PROD = %d\n",PROD);
+    
+    
+ 
+    return 0;
+}
